25a9ab02b9d5859f92c48c272955a79495c25588
Shia <rise.shia@gmail.com>
2026-03-01 00:41:33 +0900

    reorder

--- lib/activerecord_trilogy_failover.rb
+++ lib/activerecord_trilogy_failover.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 require_relative "activerecord_trilogy_failover/version"
-require_relative "activerecord_trilogy_failover/railtie" if defined?(Rails::Railtie)
 require_relative "activerecord_trilogy_failover/adapter_patch"
+require_relative "activerecord_trilogy_failover/railtie" if defined?(Rails::Railtie)
